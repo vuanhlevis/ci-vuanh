5175b1d475e6dba83fca3cce7808a854ae041654
DESKTOP-52NLMLM\VALV <vuanhlevis@gmail.com>
2017-07-09 22:31:12 +0700

    vẽ game background backBuffered game loop

---
+++ src/buoi2.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by VALV on 7/9/2017.
+ */
+public class buoi2 {
+}
---
+++ src/project_Game.java
@@ -0,0 +1,13 @@
+import Game.GameWindow;
+
+/**
+ * Created by VALV on 7/9/2017.
+ */
+public class project_Game {
+    public static void main(String[] args) {
+        // tao WindowGame
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.Run();
+    }
+}
+
